575c396d2fd454f2eecbf172279eee25b570cd1d
huyleemf <hoangduchuy03121999@gmail.com>
2021-08-05 16:15:38 +0700

    fix bug

--- Souces/Screen/Home/index.js
+++ Souces/Screen/Home/index.js
@@ -1,57 +1,66 @@
-import React, { useEffect, useState } from "react";
-import { ImageBackground, ScrollView, Text, View } from "react-native";
-import { heightScreen, widthScreen } from "../../CommonFunction";
-import { AppText } from "../../Components";
+import React, {useEffect, useState} from 'react';
+import {ImageBackground, ScrollView, Text, View} from 'react-native';
+import {heightScreen, widthScreen} from '../../CommonFunction';
+import {AppText} from '../../Components';
 
-import AppCircleTime from "../../Components/AppCircleTime";
-import styleCommon from "../../CSS/common/styleCommon";
-import style from "../../CSS/HourView/style";
-import ChartWeather from "./ChartWeather";
-import Header from "./Header";
-import HoursView from "./HoursView";
-import HumidityView from "./HumidityView";
-import WinSpeed from "./WinSpeed";
-import { getCurrentLocation, getCurrentLocationByIP, getWeather } from "../../Api/CallApi";
-import { isEmpty } from "lodash";
+import AppCircleTime from '../../Components/AppCircleTime';
+import styleCommon from '../../CSS/common/styleCommon';
+import style from '../../CSS/HourView/style';
+import ChartWeather from './ChartWeather';
+import Header from './Header';
+import HoursView from './HoursView';
+import HumidityView from './HumidityView';
+import WinSpeed from './WinSpeed';
+import {
+  getCurrentLocation,
+  getCurrentLocationByIP,
+  getWeather,
+} from '../../Api/CallApi';
+import {isEmpty} from 'lodash';
 
-export default function ({ navigation }) {
+export default function ({navigation}) {
   const [currenLocation, setCurrentLocation] = useState();
   const [inforDay, setInforDay] = useState({});
-  console.log(inforDay, "inforDay");
+  console.log(inforDay, 'inforDay');
   useEffect(() => {
-    getCurrentLocation().then((response) => {
-      getCurrentLocationByIP(response.IPv4).then((e) => {
+    getCurrentLocation().then(response => {
+      getCurrentLocationByIP(response.IPv4).then(e => {
         setCurrentLocation(e);
       });
     });
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (isEmpty(currenLocation)) return;
-    getWeather(currenLocation.ParentCity.Key).then((res) => {
+    getWeather(currenLocation.ParentCity.Key).then(res => {
       setInforDay(res.DailyForecasts[0]);
     });
-  }, [currenLocation])
+  }, [currenLocation]);
 
-  const convertTemp = (f) => {
-    return parseInt((f - 32) * 0.555)
-  }
+  const convertTemp = f => {
+    return parseInt((f - 32) * 0.555);
+  };
   return (
-    <ImageBackground source={require("../../Asset/Image/background.png")} style={{ width: widthScreen, height: heightScreen }}>
+    <ImageBackground
+      source={require('../../Asset/Image/background.png')}
+      style={{width: widthScreen, height: heightScreen}}>
       <ScrollView
         showsVerticalScrollIndicator={false}
         scrollToOverflowEnabled
         scrollEnabled
-        style={{ height: heightScreen }}
-      >
+        style={{height: heightScreen}}>
         <Header
           location={{
-            name: currenLocation?.ParentCity?.LocalizedName || "",
-            t: parseInt((convertTemp(inforDay?.Temperature?.Maximum?.Value) + convertTemp(inforDay?.Temperature?.Minimum?.Value)) / 2),
+            name: currenLocation?.ParentCity?.LocalizedName || '',
+            t: parseInt(
+              (convertTemp(inforDay?.Temperature?.Maximum?.Value) +
+                convertTemp(inforDay?.Temperature?.Minimum?.Value)) /
+                2,
+            ),
             tMax: convertTemp(inforDay?.Temperature?.Maximum?.Value) || 0,
             tMin: convertTemp(inforDay?.Temperature?.Minimum?.Value) || 0,
             doAm: 23,
-            mota: "Có Mây",
+            mota: 'Có Mây',
           }}
         />
         <View style={[styleCommon.containerComponent]}>
@@ -59,11 +68,12 @@ export default function ({ navigation }) {
         </View>
 
         <View>
-          <View
-            style={[style.containerHourView]}
-          >
+          <View style={[style.containerHourView]}>
             <AppText>Dự báo hằng ngày</AppText>
             <AppText>25 ngày</AppText>
+          </View>
+          <View style={styleCommon.containerComponent}>
+            <HoursView />
           </View>
           <View style={styleCommon.containerComponent}>
             <HoursView />
